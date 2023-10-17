24dc979a0d787dfab03f0f1af8a066388592a2d8
Banksy5m <147591332+Banksy5m@users.noreply.github.com>
2023-10-17 18:48:58 +0100

    add: ksd stream files

--- cfg_garages.lua
+++ cfg_garages.lua
@@ -1428,6 +1428,7 @@ cfg.garages = {
 		["tr87"] = {"Ferrari Testarossa Avante Widebody",100000000,""},
 		["kr200"] = {"Dinger",100000000,""},
 		["q8hycade"] = {"Audi Q8 Hycade",100000000,""},
+		["ksd"] = {"Surron Light Bee",100000000,""},
 	},
   	["Standard Boats"] = {
 		["_config"] = {blipid=427,blipcolor=7,markerid=35,markercolours={0,200,0},permissions={},type="sea"},
